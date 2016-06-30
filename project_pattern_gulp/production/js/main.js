a6581627e00ae416eccce94c21029afc51611f1c
Oleksandr Madi <o.madi@astoundcommerce.com>
2016-06-30 15:03:53 +0300

    first commit

---
+++ project_pattern_gulp/production/js/main.js
@@ -0,0 +1,112 @@
+$(document).ready(function(){
+	
+
+});
+
+
+
+
+
+
+
+/*
+
+$('.projects_carousel.owl-carousel').owlCarousel({
+	autoplay: true,
+	nav: true, 
+	mouseDrag: false,
+	loop: true, 
+
+	responsiveClass:true,
+    responsive:{
+        0:{
+            items: 1,
+            nav: false,
+            mouseDrag: true,
+            stagePadding: 50,
+            margin: 10, 
+        },
+        768:{ 
+            items:3,
+        },
+        1000:{
+            items:4,
+            nav:true,
+            
+        }
+    }
+});
+$('.project_carousel.owl-carousel').owlCarousel({
+	autoplay: false,
+	items: 1,
+	nav: true,
+	mouseDrag: false, 
+	loop: true,
+
+	responsive:{
+        0:{
+          // autoplay: false,
+          touchDrag: false,
+          mouseDrag: false,
+
+        },
+        768:{ 
+            
+        },
+        1000:{
+            
+            
+        }
+    }
+
+});
+
+$('.slider.owl-carousel').owlCarousel({
+	autoplay: false,
+	items: 1,
+	nav: true,
+	mouseDrag: true, 
+	loop:true,
+
+	responsive:{
+        0:{
+          // autoplay: false,
+           stagePadding: 40,
+
+        },
+        768:{ 
+            
+        },
+        1000:{				            
+            
+        }
+    },
+
+
+
+});
+$('.faq_slider.owl-carousel').owlCarousel({
+	autoplay: true,
+	items: 1,
+	nav: true,
+	mouseDrag: false, 
+	loop:true,
+
+	responsive:{
+        0:{
+          // autoplay: false,
+           stagePadding: 40, 
+
+        },
+        768:{ 
+            
+        },
+        1000:{				            
+            
+        }
+    }
+
+});
+
+
+*/
